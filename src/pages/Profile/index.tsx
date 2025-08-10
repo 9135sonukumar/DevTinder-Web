7334e0a7808fd92c7c9f5ffc814920f8a8b5ee4f
Sonu Kumar <sonukumarbgs9135@gmail.com>
2025-08-10 17:21:25 +0530

    1. Add Footer component 2. Integrate React router dom 3. Create Component for Login & Profile

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,9 @@
+const Profile = () => {
+  return (
+    <div className="flex flex-col sm:flex-row min-h-screen">
+      <h1 className="text-3xl font-bold">Profile Page</h1>
+    </div>
+  );
+};
+
+export default Profile;
